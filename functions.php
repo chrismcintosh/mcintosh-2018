d171b372249a02684636c94ddadbe3ed24bc8072
Chris Mcintosh <chris@mcintosh.io>
2017-12-14 16:22:40 -0500

    pre webpack set up

--- functions.php
+++ functions.php
@@ -16,13 +16,6 @@ if ( ! function_exists( 'mcintosh_setup' ) ) :
 	 * as indicating support for post thumbnails.
 	 */
 	function mcintosh_setup() {
-		/*
-		 * Make theme available for translation.
-		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on mcintosh-18, use a find and replace
-		 * to change 'mcintosh' to the name of your theme in all the template files.
-		 */
-		load_theme_textdomain( 'mcintosh', get_template_directory() . '/languages' );
 
 		// Add default posts and comments RSS feed links to head.
 		add_theme_support( 'automatic-feed-links' );
@@ -119,9 +112,9 @@ add_action( 'widgets_init', 'mcintosh_widgets_init' );
 function mcintosh_scripts() {
 	wp_enqueue_style( 'mcintosh-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'mcintosh-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'mcintosh-navigation', get_template_directory_uri() . '/assets/js/default/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'mcintosh-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'mcintosh-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/default/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -132,33 +125,33 @@ add_action( 'wp_enqueue_scripts', 'mcintosh_scripts' );
 /**
  * Implement the Custom Header feature.
  */
-require get_template_directory() . '/inc/custom-header.php';
+require get_template_directory() . '/lib/inc/custom-header.php';
 
 /**
  * Custom template tags for this theme.
  */
-require get_template_directory() . '/inc/template-tags.php';
+require get_template_directory() . '/lib/inc/template-tags.php';
 
 /**
  * Functions which enhance the theme by hooking into WordPress.
  */
-require get_template_directory() . '/inc/template-functions.php';
+require get_template_directory() . '/lib/inc/template-functions.php';
 
 /**
  * Customizer additions.
  */
-require get_template_directory() . '/inc/customizer.php';
+require get_template_directory() . '/lib/inc/customizer.php';
 
 /**
  * Load Jetpack compatibility file.
  */
 if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
+	require get_template_directory() . '/lib/inc/jetpack.php';
 }
 
 /**
  * Load WooCommerce compatibility file.
  */
 if ( class_exists( 'WooCommerce' ) ) {
-	require get_template_directory() . '/inc/woocommerce.php';
+	require get_template_directory() . '/lib/inc/woocommerce.php';
 }
