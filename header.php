9e21c2912f780df7d371795db15ef9e2413114eb
Chris Mcintosh <chris@mcintosh.io>
2017-12-15 16:09:45 -0500

    done for the day

--- header.php
+++ header.php
@@ -25,30 +25,27 @@
 
 	<header class="site-header">
 		<div class="site-header__wrapper">
-				<h1 class="site-title" rel="home">
-
-						<?php 
-							if ( has_custom_logo() === true ) { 
-								the_custom_logo();
-							} else {
-								?> <a href="<?php echo esc_url( home_url( '/' ) ); ?>"> <?php bloginfo( 'name' );?> </a><?php
-							}
-						?>
-				</h1>
+			<h1 class="site-title" rel="home">
+				<?php 
+					if ( has_custom_logo() === true ) { 
+						the_custom_logo();
+					} else {
+						?> <a href="<?php echo esc_url( home_url( '/' ) ); ?>"> <?php bloginfo( 'name' );?> </a><?php
+					}
+				?>
+			</h1>
 		<nav id="site-navigation" class="main-navigation">
 			<button class="mobile-menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'mcintosh' ); ?></button>
 			<?php
 				wp_nav_menu( array(
 					'theme_location' => 'menu-1',
 					'menu_id'        => 'primary-menu',
-					'items_wrap' => '<ul id="%1$s" class="%2$s menu dropdown vertical medium-horizontal" data-responsive-menu="accordion medium-dropdown">%3$s</ul>',
+					'items_wrap' => '<ul id="%1$s" class="%2$s dropdown vertical medium-horizontal" data-responsive-menu="accordion medium-dropdown">%3$s</ul>',
 					'walker'  => new Foundation_Walker()
 				) );
 			?>
 		</nav><!-- #site-navigation -->
-		</div><!-- .site-branding -->
-
-
-	</header><!-- #masthead -->
+		</div><!-- .site-header__wrapper -->
+	</header>
 
 	<div id="content" class="site-content">
