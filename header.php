aeb9ee083d4c747ddb29c36011cf1738ff22d5ce
Chris Mcintosh <chris@mcintosh.io>
2017-12-15 15:41:40 -0500

    get started on header

--- header.php
+++ header.php
@@ -23,24 +23,18 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'mcintosh' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
+	<header class="site-header">
+		<div class="site-header__wrapper">
+				<h1 class="site-title" rel="home">
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
-
+						<?php 
+							if ( has_custom_logo() === true ) { 
+								the_custom_logo();
+							} else {
+								?> <a href="<?php echo esc_url( home_url( '/' ) ); ?>"> <?php bloginfo( 'name' );?> </a><?php
+							}
+						?>
+				</h1>
 		<nav id="site-navigation" class="main-navigation">
 			<button class="mobile-menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'mcintosh' ); ?></button>
 			<?php
@@ -52,6 +46,9 @@
 				) );
 			?>
 		</nav><!-- #site-navigation -->
+		</div><!-- .site-branding -->
+
+
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
